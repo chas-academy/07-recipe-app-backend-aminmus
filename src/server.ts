3d27a1cd0b00968b5b5690b64b36070fbe60d730
Amin Mushehid <amin.mushehid@gmail.com>
2019-09-08 18:03:04 +0200

    Make initial server.ts file and modify project configuration.

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,8 @@
+require('dotenv').config();
 import express from 'express';
 
-const app = express();
+const app: express.Application = express();
+
+app.listen(process.env.PORT, () => {
+  console.log('✅  Server is running at http://localhost:%s', process.env.PORT,);  
+});
