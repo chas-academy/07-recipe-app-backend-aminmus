14b3c09ebf18b174daabab33ed3db9e5be0ad734
Amin Mushehid <amin.mushehid@gmail.com>
2019-09-09 16:45:24 +0200

    Modify server.ts and add draft recipe controller with comments.
    Also allow no-console rule in eslint.

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
-require('dotenv').config();
-import express from 'express';
+import express, { Application } from 'express';
 
-const app: express.Application = express();
+require('dotenv').config();
+
+const app: Application = express();
 
 app.listen(process.env.PORT, () => {
-  console.log('✅  Server is running at http://localhost:%s', process.env.PORT,);  
+  console.log('✅  Server is running at http://localhost:%s', process.env.PORT);
 });
