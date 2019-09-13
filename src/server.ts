b0fbab2056ed0d3d44699e4ae804247a2fecc130
Amin Mushehid <amin.mushehid@gmail.com>
2019-09-13 19:36:08 +0200

    Modify server to use graphql-yoga and add basic graphql nexus schema.

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,30 @@
-import express, { Application } from 'express';
+import { GraphQLServer } from 'graphql-yoga';
+import { makeSchema, queryType, stringArg } from 'nexus';
+import path from 'path';
 
 require('dotenv').config();
 
-const app: Application = express();
+const Query = queryType({
+  definition(t) {
+    t.string('hello', {
+      args: { name: stringArg({ nullable: true }) },
+      resolve: (_parent, { name }) => `Hello ${name || 'World'}!`,
+    });
+  },
+});
 
-app.listen(process.env.PORT, () => {
-  console.log('✅  Server is running at http://localhost:%s', process.env.PORT);
+const schema = makeSchema({
+  types: [Query],
+  outputs: {
+    schema: path.join(__dirname, './generated/schema.graphql'),
+    typegen: path.join(__dirname, './generated/typings.ts'),
+  },
+});
+
+const server = new GraphQLServer({
+  schema,
+});
+
+server.start({ port: process.env.PORT }, () => {
+  console.log('Server is running at http://localhost:%s', process.env.PORT);
 });
