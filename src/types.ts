e77adce5e68d1e66f09c2810f4b6e34a3379c809
Amin Mushehid <amin.mushehid@gmail.com>
2019-09-16 16:30:42 +0200

    Add more specifics to interfaces.

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,17 @@
 export interface Recipe {
-  label: string;
   uri: string;
+  label: string;
+  image?: string;
+  yield?: number;
+  calories?: number;
+  totalWeight?: number;
+  totalNutrients?: object;
+  ingredients?: Array<object>;
+  dietLabels?: Enumerator<string>;
+  healthLabels?: Enumerator<string>;
+}
+
+export interface SearchFilter {
+  mealType: Enumerator<string>;
+  health: Array<string>;
 }
