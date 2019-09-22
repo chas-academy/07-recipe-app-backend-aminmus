f619717aa55ad25c4e7be21c019342546061ac8d
Amin Mushehid <amin.mushehid@gmail.com>
2019-09-22 05:13:59 +0200

    Replace Recipe resolver with a specific EdamamRecipe resolver for external API calls.

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -1,9 +1,9 @@
 import { queryType, stringArg, arg } from 'nexus';
 
-import Recipe from './Recipe';
+// import Recipe from './Recipe';
 import { Filters } from './Filters';
 import { searchRecipes, findRecipeByURI } from '../services/getRecipes';
-import getUserId from '../utils/getUserId';
+import EdamamRecipe from './EdamamRecipe';
 
 const Query = queryType({
   definition(t) {
@@ -11,12 +11,13 @@ const Query = queryType({
       args: { name: stringArg({ nullable: true }) },
       resolve: (_parent, { name }) => `Hello ${name || 'World'}!`,
     });
+
     t.string('testProtected', {
-      resolve: (_parent, _args, context) => 'Only authenticated requests should see this response that you are reading now',
+      resolve: () => 'Only authenticated requests should see this response that you are reading now',
     });
 
     t.list.field('searchRecipes', {
-      type: Recipe,
+      type: EdamamRecipe,
       description: 'Search for recipes',
       nullable: true,
       args: {
@@ -27,7 +28,7 @@ const Query = queryType({
     });
 
     t.field('findRecipeByURI', {
-      type: Recipe,
+      type: EdamamRecipe,
       description: 'Find a recipe by uri',
       nullable: true,
       args: { uri: stringArg({ nullable: false }) },
